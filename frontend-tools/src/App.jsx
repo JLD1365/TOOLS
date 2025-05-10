212f5f5417ed9adfca3c2dbba753f9537b378f09
jld1365 <jldev1365@gmail.com>
2025-05-10 20:50:37 +0800

    refactor: remove Link wrapper around EWTCALCULATOR component

--- frontend-tools/src/App.jsx
+++ frontend-tools/src/App.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function App() {
   return (
     <div className="">
-      <Link to="/ewt">{<EWTCALCULATOR />}</Link>
+      <div>{<EWTCALCULATOR />}</div>
     </div>
   )
 }
