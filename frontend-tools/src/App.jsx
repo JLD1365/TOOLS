bb333305ee974b2c097275ebed02bcd2afefb8ce
jld1365 <jldev1365@gmail.com>
2025-05-10 20:48:32 +0800

    refactor: simplify routing by replacing Routes and Route with a direct Link to EWTCALCULATOR

--- frontend-tools/src/App.jsx
+++ frontend-tools/src/App.jsx
@@ -1,12 +1,10 @@
-import {Routes, Route} from 'react-router-dom';
 import EWTCALCULATOR from './components/EWTCALCULATOR';
+import { Link } from 'react-router-dom';
 
 export default function App() {
   return (
     <div className="">
-      <Routes>
-        <Route path='/ewt' element={<EWTCALCULATOR/>}/>
-      </Routes>
+      <Link to="/ewt">{<EWTCALCULATOR />}</Link>
     </div>
   )
 }
